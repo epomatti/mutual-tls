95aa63ef7a689df309d318136f4a65ffd461d354
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-02-22 12:44:50 -0300

    spring boot server

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ mvn exec:java
 mvn versions:display-dependency-updates
 mvn versions:display-plugin-updates
 ```
+
+```
+./mvnw spring-boot:run
+```
---
+++ server/src/test/java/io/pomatti/mtls/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pomatti.mtls.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
