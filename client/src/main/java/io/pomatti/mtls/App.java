98855ded372974e75943d45c1a6c651f1d4c29e6
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-02-22 12:34:11 -0300

    http client

--- client/src/main/java/io/pomatti/mtls/App.java
+++ client/src/main/java/io/pomatti/mtls/App.java
@@ -1,9 +1,21 @@
 package io.pomatti.mtls;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create("http://openjdk.org/"))
+        .build();
+
+    HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+
+    System.out.println(response.statusCode());
+    System.out.println(response.body());
+  }
 }
